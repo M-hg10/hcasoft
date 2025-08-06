7ff74894abc6d034f363202886084933b9ca6e5b
halil gezer <hg_1993@windowslive.com>
2025-08-07 00:11:46 +0300

    cors eklemesi yapıldı

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
+
 
 // ✅ JSON body verisini almak için middleware
 app.use(express.json());
